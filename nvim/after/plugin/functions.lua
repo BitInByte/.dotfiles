44e66704e1d772f3a9fbfceb6f18011115eecbcd
bitinbyte <bitinbyte@outlook.com>
2022-11-08 19:07:01 +0000

    Added highlight on yank

---
+++ nvim/after/plugin/functions.lua
@@ -0,0 +1,11 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", { clear = true })
+
+autocmd("TextYankPost", {
+	group = yank_group,
+	-- pattern = '*',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
