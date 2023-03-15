9e0b685d29cdf4f7c17f2b11cf40e37df5c1f05c
bitinbyte <bitinbyte@outlook.com>
2023-03-15 11:37:21 +0100

    Added some fixes and lazygit config changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,3 +15,5 @@ require("plugins") -- lazy plugins manager
 require("core") -- core functionality such as (LSP, CMP and so on)
 require("modules") -- util plugins
 require("themes") -- colorscheme
+
+vim.opt.laststatus = 3 -- Status bar always on bottom (shared)
