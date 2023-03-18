d2fb613638ec215ea1ab521fcb690047dbb45e08
bitinbyte <bitinbyte@outlook.com>
2023-03-18 21:07:39 +0100

    Fixed issues on loading file that doesn't exist

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,6 @@ vim.g.mapleader = " "
 require("plugins") -- lazy plugins manager
 -- require("core") -- core functionality such as (LSP, CMP and so on)
 -- require("modules") -- util plugins
-require("themes") -- colorscheme
+-- require("themes") -- colorscheme
 
 vim.opt.laststatus = 3 -- Status bar always on bottom (shared)
