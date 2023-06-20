0e900a846a22a6a3105277411388315485ae4c5c
bitinbyte <bitinbyte@outlook.com>
2023-06-20 12:36:35 +0100

    Fix: luasnip issue because of version

--- nvim/lua/modules/nv-luasnip/init.lua
+++ nvim/lua/modules/nv-luasnip/init.lua
@@ -1,20 +1,20 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-        config = function()
-            local luasnip = require("luasnip")
-            require("luasnip.loaders.from_vscode").lazy_load()
-            -- luasnip.filetype_extend("javascript", { "javascriptreact" })
-            luasnip.filetype_extend("typescript", { "typescriptreact" })
-            luasnip.filetype_extend("php", { "html" })
-        end,
-    },
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		-- version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			local luasnip = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
+			-- luasnip.filetype_extend("javascript", { "javascriptreact" })
+			luasnip.filetype_extend("typescript", { "typescriptreact" })
+			luasnip.filetype_extend("php", { "html" })
+		end,
+	},
 }
