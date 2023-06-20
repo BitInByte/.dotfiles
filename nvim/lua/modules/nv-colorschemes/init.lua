4bcd2feaa0b64840ed66f7facfe8853f0c5ada8a
bitinbyte <bitinbyte@outlook.com>
2023-06-20 12:35:58 +0100

    Fix: nightfly wrong lazy object

--- nvim/lua/modules/nv-colorschemes/init.lua
+++ nvim/lua/modules/nv-colorschemes/init.lua
@@ -1,5 +1,6 @@
 return {
-	{ "bluz71/vim-nightfly-colors", lazy = true },
+	-- { "bluz71/vim-nightfly-colors", lazy = true },
+	{ "bluz71/vim-nightfly-colors", name = "nightfly", lazy = true, priority = 1000 },
 	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
 	{ "Mofiqul/dracula.nvim", lazy = true },
 }
