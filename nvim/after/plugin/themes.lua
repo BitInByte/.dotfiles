23ae46e9a9f48972f2f455254152dea2e495a6e2
bitinbyte <bitinbyte@outlook.com>
2023-06-20 12:35:03 +0100

    Add: changed theme

--- nvim/after/plugin/themes.lua
+++ nvim/after/plugin/themes.lua
@@ -1,5 +1,7 @@
 -- vim.cmd [[colorscheme nightfly]]
 -- vim.cmd([[colorscheme catppuccin_macchiato]])
 -- vim.cmd.colorscheme("catppuccin")
-vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("catppuccin")
+vim.g.nightflyVirtualTextColor = true
+vim.cmd([[colorscheme nightfly]])
 -- vim.cmd.colorscheme("dracula")
