fbec42a73d8db40cd254262296a8aa0629289a7f
bitinbyte <bitinbyte@outlook.com>
2023-06-22 20:01:54 +0100

    Add: convert to event instead of on_attach function

--- nvim/lua/core/lsp/init.lua
+++ nvim/lua/core/lsp/init.lua
@@ -17,45 +17,66 @@ keymap.set("n", "[d", vim.diagnostic.goto_prev, mappingOpts)
 keymap.set("n", "]d", vim.diagnostic.goto_next, mappingOpts)
 keymap.set("n", "<space>q", vim.diagnostic.setloclist, mappingOpts)
 
--- Use an on_attach function to only map the following keys
+-- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-	if client.server_capabilities.documentSymbolProvider then
-		navic.attach(client, bufnr)
-	end
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local bufnr = ev.buf
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client.server_capabilities.documentSymbolProvider then
+			navic.attach(client, bufnr)
+		end
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	-- keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	keymap.set("n", "gd", builtin.lsp_definitions, bufopts)
-	keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, bufopts)
-	keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-	keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	keymap.set("n", "<space>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-	-- keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	keymap.set("n", "gr", builtin.lsp_references, bufopts)
-	keymap.set("n", "<space>bf", function()
-		-- vim.lsp.buf.format({ async = false })
-		vim.lsp.buf.format({
-			async = false,
-			filter = function(cli)
-				return cli.name == "null-ls"
-			end,
-		})
-	end, bufopts)
-	-- keymap.set("n", "<space>lA", vim.lsp.buf.format, bufopts)
-	vim.cmd([[
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local bufopts = { buffer = ev.buf }
+		keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+		-- keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+		keymap.set("n", "gd", builtin.lsp_definitions, bufopts)
+		keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+		keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+		keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, bufopts)
+		keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+		keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+		keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, bufopts)
+		keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+		keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+		keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+		-- keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+		keymap.set("n", "gr", builtin.lsp_references, bufopts)
+		keymap.set("n", "<space>bf", function()
+			-- vim.lsp.buf.format({ async = false })
+			vim.lsp.buf.format({
+				async = false,
+				filter = function(cli)
+					return cli.name == "null-ls"
+				end,
+			})
+		end, bufopts)
+		-- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		-- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+		-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		-- vim.keymap.set('n', '<space>wl', function()
+		--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		-- end, opts)
+		-- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+		-- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+		-- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+		-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		-- vim.keymap.set('n', '<space>f', function()
+		--   vim.lsp.buf.format { async = true }
+		-- end, opts)
+
+		vim.cmd([[
             augroup lsp_document_highlight
                 autocmd! * <buffer>
                 " autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
@@ -65,7 +86,8 @@ local on_attach = function(client, bufnr)
                   autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
             augroup END
         ]])
-end
+	end,
+})
 
 local lsp_flags = {
 	-- This is the default in Nvim 0.7+
@@ -89,6 +111,7 @@ local lsps_table = {
 	php_lsp = require(providers_path .. "php"),
 	tailwind_lsp = require(providers_path .. "tailwind"),
 	emmet_lsp = require(providers_path .. "emmet"),
+	go_lsp = require(providers_path .. "go"),
 }
 
 for key, _ in next, lsps_table, nil do
