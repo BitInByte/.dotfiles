afd0e7e11e8bb753b409cb8d479acdfe480f92bc
bitinbyte <bitinbyte@outlook.com>
2023-06-22 20:01:36 +0100

    Add: go lsp

--- nvim/lua/core/null-ls/init.lua
+++ nvim/lua/core/null-ls/init.lua
@@ -40,6 +40,7 @@ null_ls.setup({
 		formatting.phpcsfixer,
 		formatting.blade_formatter,
 		formatting.prettier,
+		formatting.gofumpt,
 		-- require("typescript.extensions.null-ls.code-actions"),
 	},
 	-- you can reuse a shared lspconfig on_attach callback here
