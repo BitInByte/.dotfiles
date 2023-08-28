6f60f84e7384409012843720b504f552819cef79
bitinbyte <bitinbyte@outlook.com>
2023-08-28 19:46:41 +0100

    Add: missing dependency for the context treesitter

--- nvim/lua/modules/nv-treesitter/init.lua
+++ nvim/lua/modules/nv-treesitter/init.lua
@@ -1,12 +1,14 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            "nvim-treesitter/nvim-tree-docs",
-        },
-        build = ":TSUpdate",
-        config = function()
-            require("core.treesitter") -- colorscheme util
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-tree-docs",
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		build = ":TSUpdate",
+		config = function()
+			-- Parser for context awareness
+			require("core.treesitter") -- colorscheme util
+		end,
+	},
 }
