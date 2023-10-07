41fa9599da8922f7a303afddcee4855833a7ff2a
bitinbyte <bitinbyte@outlook.com>
2023-10-07 16:39:15 +0100

    Fix: issue with context commentstring and added incremental treesitter select

--- nvim/lua/core/treesitter/init.lua
+++ nvim/lua/core/treesitter/init.lua
@@ -39,19 +39,28 @@ require("nvim-treesitter.configs").setup({
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
-		context_commentstring = {
-			enable = true,
-			enable_autocmd = false,
-		},
 	},
 	autotag = {
 		enable = true,
 		enable_close_on_slash = false,
 	},
 	tree_docs = { enable = true, spec_config = { jsdoc = {} } },
-	-- indent = {
-	-- 	enable = true,
-	-- },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn", -- set to `false` to disable one of the mappings
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+	indent = {
+		enable = true,
+	},
 })
 
 require("treesitter-context").setup({
