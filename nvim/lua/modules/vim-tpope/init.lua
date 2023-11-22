21ae1a93684196fb2fdcab71bd1b4cc6ee4a7208
bitinbyte <bitinbyte@outlook.com>
2023-11-22 17:54:33 +0000

    Add vim sleuth from king tpope

--- nvim/lua/modules/vim-tpope/init.lua
+++ nvim/lua/modules/vim-tpope/init.lua
@@ -4,4 +4,7 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 	},
 	{ "tpope/vim-surround", event = { "BufReadPre", "BufNewFile" } },
+	-- Fix indentation (tabs) based on buffer document or project in
+	-- case is new file
+	{ "tpope/vim-sleuth", event = { "BufReadPre", "BufNewFile" } },
 }
