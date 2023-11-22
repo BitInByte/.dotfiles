8830864c5d052add4ddafedb76706a33ccacde58
bitinbyte <bitinbyte@outlook.com>
2023-11-22 17:54:21 +0000

    Add twilight plugin

---
+++ nvim/lua/modules/nv-twilight/init.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/twilight.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{ "<leader>tt", "<cmd>Twilight<CR>", desc = "Toggle twilight" },
+	},
+}
