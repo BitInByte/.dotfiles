605de7f148c5ea74d8582e1d5cbe564c4a5d9d8e
bitinbyte <bitinbyte@outlook.com>
2023-11-22 17:52:47 +0000

    Add missing catppuccin themes on different plugins (for instance navic)

--- nvim/lua/modules/nv-colorschemes/init.lua
+++ nvim/lua/modules/nv-colorschemes/init.lua
@@ -1,6 +1,26 @@
 return {
 	-- { "bluz71/vim-nightfly-colors", lazy = true },
 	{ "bluz71/vim-nightfly-colors", name = "nightfly", lazy = true, priority = 1000 },
-	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		opts = {
+			integrations = {
+				mason = true,
+				noice = true,
+				navic = {
+					enabled = true,
+					-- custom_bg = "NONE", -- "lualine" will set background to mantle
+				},
+				notify = true,
+				-- treesitter_context = true,
+				lsp_trouble = true,
+				which_key = true,
+				fidget = true,
+				hop = true,
+			},
+		},
+	},
 	{ "Mofiqul/dracula.nvim", lazy = true },
 }
